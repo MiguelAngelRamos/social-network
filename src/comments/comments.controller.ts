a2941f612a0b1df73801becb7529fc32e9038d1f
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-27 18:34:10 -0400

    modulo comentarios

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { CommentsService }  from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileStorage } from './interceptors/file-storage.interceptor';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private readonly commentsService:CommentsService){}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file', {
+    storage: fileStorage
+  }))
+  // @UploadedFile() acceder al archivo subido
+  create(@Body() createCommentDto: CreateCommentDto, @UploadedFile() file: Express.Multer.File) {
+    if(file) {
+      createCommentDto.imageUrl = file.filename;
+    }
+    return this.commentsService.create(createCommentDto);
+  }
+}
